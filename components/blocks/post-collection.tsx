1505e0f0aae0e54392f848b3ee755cee59a4a343
atglenn <adam.glenn@gmail.com>
2023-09-30 15:20:53 -0400

    Deleting stuff.

--- components/blocks/post-collection.tsx
+++ components/blocks/post-collection.tsx
@@ -5,23 +5,32 @@ import { useTheme } from "../layout";
 import type { TinaTemplate } from "tinacms";
 import {
   PageBlocksPostCollection,
-  PageBlocksPostCollectionPostsPost,
+  PageBlocksPostCollectionPosts,
 } from "../../tina/__generated__/types";
 import { tinaField } from "tinacms/dist/react";
 
-export const PostCollectionPost = ({ data }: { data: PageBlocksPostCollectionPostsPost }) => {
+export const PostCollectionPost = ({ data }: {
+  data: {
+    __typename: PageBlocksPostCollectionPosts;
+    post: {
+      heroImg: string;
+      title: string;
+      preview: string;
+    }
+  }
+}) => {
   return (
     <div>
-      {data.heroImg && (
-        (data.heroImg && (
-          <figure className="mb-4"><img src={data.heroImg} /></figure>
+      {data.post.heroImg && (
+        (data.post.heroImg && (
+          <figure className="mb-4"><img src={data.post.heroImg} /></figure>
         ))
       )}
-      {data.title && (
-        <h2 className="w-100 text-2xl font-iowan-old-style-black leading-iowan-old-style text-wvu-blue mb-4">{data.title}</h2>
+      {data.post.title && (
+        <h2 className="w-100 text-2xl font-iowan-old-style-black leading-iowan-old-style text-wvu-blue mb-4">{data.post.title}</h2>
       )}
-      {data.preview && (
-        <p className="mb-4">{data.preview}</p>
+      {data.post.preview && (
+        <p className="mb-4">{data.post.preview}</p>
       )}
       <a className="text-blue" href="#">
         Read More <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" className="ml-1 -mr-1 w-6 h-6 opacity-80" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="m11.293 17.293 1.414 1.414L19.414 12l-6.707-6.707-1.414 1.414L15.586 11H6v2h9.586z"></path></svg>
@@ -41,11 +50,7 @@ export const PostCollection = ({ data }: { data: PageBlocksPostCollection }) => 
           <h2 className="font-wvu-shout leading-wvu-shout text-6xl text-wvu-blue wvu-bar mb-12">My {data.title}</h2>
         )}
         <div className="grid grid-cols-1 md:grid-cols-3 gap-14 justify-center">
-          {data.posts &&
-            data.posts.map(function (block, i) {
-              return (<PostCollectionPost key={i} data={block} />)
-            })
-          }
+          <PostCollectionPost data={data.posts} />
         </div>
       </Container>
     </Section>
